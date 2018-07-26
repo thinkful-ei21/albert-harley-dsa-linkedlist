c005e5179d62bf2c66c5eba2501346a1f7da2669
harleyjj <harleyyesm@gmail.com>
2018-07-26 09:04:11 -0700

    implemented Linked List

--- linkedlist.js
+++ linkedlist.js
@@ -10,22 +10,71 @@ class _Node {
 class LinkedList {
   
   constructor() {
-
+    this.head = null;
   }
 
   insertFirst(item) {
-
+    this.head = new _Node(item, this.head);
   }
 
   insertLast(item) {
 
-  }
-
-  remove(item) {
+    if(this.head === null) {
+      this.insertFirst(item);
+    } else {
+      let current_node = this.head;
+      while(current_node.next !== null) {
+        current_node = current_node.next;
+      }
+      current_node.next = new _Node(item, null);
+    }
 
   }
 
   find(item) {
+
+    if (this.head === null) {
+      return null;
+    }
+
+    let current_node = this.head;
+
+    while(current_node.value !== item) {
+      if (current_node.next === null) {
+        return null;
+      } else {
+        current_node = current_node.next;
+      }
+    }
+
+    return current_node;
+  }
+
+  remove(item) {
+
+    if (this.head === null) {
+      return;
+    }
+
+    if (this.head === item) {
+      this.head = this.head.next;
+      return;
+    }
+
+    let current_node = this.head;
+    let prev_node = this.head;
+
+    while (current_node.next !== null) {
+      prev_node = current_node;
+      current_node = current_node.next;
+      if (current_node === item) {
+        prev_node.next = current_node.next;
+        return;
+      }
+    }
+
+    console.log('Item not found');
+    return;
 
   }
 

4cb4b97469d40a62354d1031e14e893580ab2b49
harleyjj <harleyyesm@gmail.com>
2018-07-26 09:56:24 -0700

    creating a singly linked list

--- linkedlist.js
+++ linkedlist.js
@@ -56,7 +56,7 @@ class LinkedList {
       return;
     }
 
-    if (this.head === item) {
+    if (this.head.value === item) {
       this.head = this.head.next;
       return;
     }
@@ -67,7 +67,7 @@ class LinkedList {
     while (current_node.next !== null) {
       prev_node = current_node;
       current_node = current_node.next;
-      if (current_node === item) {
+      if (current_node.value === item) {
         prev_node.next = current_node.next;
         return;
       }
@@ -76,6 +76,86 @@ class LinkedList {
     console.log('Item not found');
     return;
 
+  }
+
+  insertBefore(item, key) {
+
+    const new_node = new _Node(item, null);
+
+    if (this.head === null) {
+      this.head = new_node;
+      return;
+    }
+
+    let current_node = this.head;
+    let prev_node = this.head;
+
+    while(current_node.value !== key) {
+      if (current_node.next === null) {
+        prev_node.next = new_node;
+        return;
+      } else {
+        prev_node = current_node;
+        current_node = current_node.next;
+      }
+    }
+
+    prev_node.next = new_node;
+    new_node.next = current_node;
+    
+  }
+
+  insertAfter(item, key) {
+
+    const new_node = new _Node(item, null);
+
+    if (this.head === null) {
+      this.head = new_node;
+      return;
+    }
+
+    let current_node = this.head;
+
+    while(current_node.value !== key) {
+      if (current_node.next === null) {
+        current_node.next = new_node;
+        return;
+      } else {
+        current_node = current_node.next;
+      }
+    }
+
+    current_node.next = new_node;
+    
+  }
+
+  insertAt(item, n) {
+    if(n === 0) {
+      this.insertFirst(item);
+      return;
+    }
+
+    let i = 0;
+    let current_node = this.head;
+    let prev_node = this.head;
+
+    if (current_node === null) {
+      console.log('Index out of range');
+      return;
+    }
+
+    while(i < n) {
+      if (current_node.next === null) {
+        console.log('Index out of range');
+        return;
+      }
+      prev_node = current_node;
+      current_node = current_node.next;
+      i++;
+    }
+
+    prev_node.next = new _Node(item, current_node.next);
+    return;
   }
 
 }
--- main.js
+++ main.js
@@ -4,6 +4,20 @@ const LinkedList = require('./linkedlist');
 
 function main() {
 
+  const SLL = new LinkedList();
+  const items = ['Apollo', 'Boomer', 'Helo', 'Husker', 'Starbuck'];
+  items.forEach(item => SLL.insertFirst(item));
+  SLL.insertFirst('Tauhida');
+  console.log(SLL);
+  SLL.remove('squirrel');
+  SLL.insertBefore('Athena','Boomer');
+  console.log(SLL);
+  SLL.insertAfter('Hotdog', 'Helo');
+  console.log(SLL);
+  SLL.insertAt('Kat', 1);
+  console.log(SLL);
+  SLL.remove('Tauhida');
+  console.log(SLL);
 }
 
 main();

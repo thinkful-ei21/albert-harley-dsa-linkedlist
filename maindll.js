33700b7dec3cef3037a3e1c4e3d8275cbd457e95
harleyjj <harleyyesm@gmail.com>
2018-07-26 14:35:59 -0700

    implemented double linked list reverse list

--- maindll.js
+++ maindll.js
@@ -17,6 +17,29 @@ function mainDLL() {
     console.log(linkedToArray(linkedlist));
   }
 
+  function reverseList(linkedlist) {
+    if(linkedlist.head === null) {
+      console.log('List is empty');
+      return;
+    }
+  
+    let current_node = linkedlist.head;
+    let temp_ptr = null;
+    
+    while(current_node) {
+      temp_ptr = current_node.prev;
+      current_node.prev = current_node.next;
+      current_node.next = temp_ptr;
+      if (current_node.prev === null) {
+        linkedlist.head = current_node;
+      }
+      if(current_node.next === null) {
+        linkedlist.tail = current_node;
+      }
+      current_node = current_node.prev;
+    }
+  }
+
   const DLL = new DoubleList();
   const items = ['Aquaria', 'Caprica', 'Gemenon', 'Picon', 'Sagittaron'];
   items.forEach(item => DLL.insertFirst(item));
@@ -24,6 +47,8 @@ function mainDLL() {
   DLL.insertAfter('Tauron', 'Gemenon');
   display(DLL);
   DLL.remove('Picon');
+  display(DLL);
+  reverseList(DLL);
   display(DLL);
 }
 
